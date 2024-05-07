e33aae9d7a7e3e2d04b713080b09eeeeecdb27fe
Crystal Huang <chuan147@ucsc.edu>
2024-05-06 17:04:43 -0700

    marker for video 3.6

--- src/World.js
+++ src/World.js
@@ -408,16 +408,23 @@ function updateAnimationAngles() {
   
 }
 
+var g_eye = [0,0,3];
+var g_at = [0,0,-100];
+var g_up = [0,1,0];
+
 function renderAllShapes() {
   // Check the time at the start of the function
   var startTime = performance.now();
 
   // makes viewing the blocks possible
   var projMat = new Matrix4();
+  projMat.setPerspective(50, 1*canvas.width/canvas.height, 1, 100);
   gl.uniformMatrix4fv(u_ProjectionMatrix, false, projMat.elements);
   
   var viewMat = new Matrix4();
-  gl.uniformMatrix4fv(u_ViewMatrix, false, viewMat.elements);
+//   viewMat.setLookAt(0,0,3, 0,0,-100, 0,1,0);
+  viewMat.setLookAt(g_eye[0],g_eye[1],g_eye[2], g_at[0], g_at[1], g_at[2], g_up[0],g_up[1],g_up[2]);
+gl.uniformMatrix4fv(u_ViewMatrix, false, viewMat.elements);
 
   // Pass the matrix to u_ModelMatrix.attribute
   var globalRotMat=new Matrix4().rotate(g_globalAngle, 0,1,0);
